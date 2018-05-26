e1ff2246d344caff0e666ee74e96b61ff7f28976
DillonHammill <dillonh21@live.com.au>
2018-05-27 08:22:54 +1000

    Update data

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
-#' fs
+#' OTI
 #'
-#' A flowSet containg two flowFrames for testing openCyto plugins.
+#' A flowSet containg two flowFrames from tryping of OT-I and OT-II transgenic T cells.
 #'
 #' @format A flowSet containing two flowFrame objects:
 #'
-"fs"
+"OTI"
