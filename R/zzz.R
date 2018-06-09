97d6c12d1f49bdaa0bbbedb6643841f67fc0936e
Dillon Hammill <dillonh21@live.com.au>
2018-06-09 16:53:56 +1000

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-#' Automatically Register DrawGate with openCyto
+#' Automatically register DrawGate and ManualGate with openCyto upon loading...
 .onLoad <- function(libname,pkgname){
   openCyto::registerPlugins(fun = gate_draw, methodName = "DrawGate")
   openCyto::registerPlugins(fun = gate_manual, methodName = "ManualGate")
